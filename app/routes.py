937e4deff29615a951a7a43274318522a0583326
Jason Strahler <jason.strahler@gmail.com>
2019-10-29 11:50:39 -0400

    Server base and gitignore

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,50 @@
 from app import app
 from flask import request
+import logging
+from logging.handlers import RotatingFileHandler
 
-@app.route('elevation')
+
+noaa_url = 'https://gis.ngdc.noaa.gov/mapviewer-support/wcs-proxy/wcs.groovy?filename=etopo1.xyz&request=getcoverage&version=1.0.0&service=wcs&coverage=etopo1&CRS=EPSG:4326&format=xyz&resx=0.016666666666666667&resy=0.016666666666666667&bbox=-98.08593749997456,36.03133177632377,-88.94531249997696,41.508577297430456'
+
+
+
+# This takes the output of the server and adds the appropriate headers to make the security team happy
+def harden_response(message_str):
+    response = app.make_response(message_str)
+    response.headers['Content-Security-Policy'] = "default-src 'self'"
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    return response
+
+@app.route('/elevation')
 def ele():
-
-    return
-
-
-def pipeline():
     try:
         input_val = [request.args['minlat'], request.args['minlon'], request.args['maxlat'], request.args['maxlon']]
 
+    except:
+        return 'not valid inputs'
 
     return
+    
 
+@app.route('/favicon.ico')
+def icon():
+    return ''
+
+@app.route('/')
+def noinput():
+    return harden_response(page_not_found())
+
+@app.errorhandler(404)
+def page_not_found(e=''):
+    return harden_response("Not a valid URL")
+
+@app.errorhandler(500)
+def server_error(e=''):
+    return harden_response("Server Error occured while attempting to process your request. Please try again...")
+
+def pipeline():
+    
+    return
 
 #setting up the server log
 format = logging.Formatter('%(asctime)s %(message)s')
